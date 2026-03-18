fd4e588a9371d9c5cd5059b9a9e3f72fba4e090e
Liviu Petrache <liviu.petrache@tss-yonder.com>
2026-03-18 14:25:40 +0200

    [training2] typo
    also: https://music.youtube.com/watch?v=5kpiZr3tTgg&list=RDAMVMAqgCV1NAbKM

--- sql/init.sql
+++ sql/init.sql
@@ -4,7 +4,7 @@ GRANT ALL PRIVILEGES ON SCHEMA stocks TO stocksuser;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA stocks TO stocksuser;
 ALTER USER stocksuser SET search_path = stocks;
 
--- separate scheam for keycloak
+-- separate schema for keycloak
 CREATE SCHEMA IF NOT EXISTS keycloak;
 CREATE USER keycloakuser WITH PASSWORD 'keycloakpassword';
 GRANT ALL PRIVILEGES ON SCHEMA keycloak TO keycloakuser;
